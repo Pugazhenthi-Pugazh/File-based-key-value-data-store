689f1621131dadbd34193ac9e45fa908eb1ec6b9
Pugazhenthi-Pugazh <pugazhenthis249@gmail.com>
2021-01-01 00:44:48 +0530

    atlast

--- key_value_data_store_lib.py
+++ key_value_data_store_lib.py
@@ -13,6 +13,7 @@ def changefilepath(path):
 def create(key,value,timeout=0):
     json_file = open(filepath, "r")
     data = json.load(json_file)
+    
     if key in data:
         print("Error: This key is already exists")
     else:
@@ -29,39 +30,46 @@ def create(key,value,timeout=0):
         else:
             print("Error: Key name is invalid, key name must in alphabets and please enter the alphbets only.")
     outfile = open(filepath, "w")
-    json.dump(data,outfile);
+    json.dump(data,outfile)
             
 def read(key):
+    json_file = open(filepath, "r")
+    data = json.load(json_file)
     if key not in data:
         print("Error: Entered key is not exist. Please enter the valid key")
     else:
         key_name = data[key]
         if key_name[1]!=0:
             if time.time()<key_name[1]:
-                string = str(key)+":"+str(key_name[0])
-                return string       
+                string = str(key)+":"+list(key_name[0])
+                return string
+                
             else:
                 print("Error: This key is expired")
         else:
-            string =  str(key)+":"+str(key_name[0])
+            string = str(key)+":"+str(key_name[0])
             return string
                     
 def delete(key):
+    
+    json_file = open(filepath, "r")
+    data = json.load(json_file)
     if key not in data:
         print("This",key,"key does not exist. Please enter the valid key")
     else:
         key_name = data[key]
         if key_name[1]!=0:
             if time.time()<key_name[1]:
-                del data[key]
-                print("key is deleted successfully")
+                    if key in data:
+                        del data[key]
+                        print("key is deleted successfully")
             else:
                 print("Error: This ",key,"key time is expired")
         else:
-            del data[key]
+            if key in data:
+                        del data[key]
             print("Key is deleted successfully")
-            
+    outfile = open(filepath, "w")
+    json.dump(data,outfile);
 
-create("oldkey",30)
-            
-
+create("pugazh",34)
